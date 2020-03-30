9fae88ab4801d0eb07328ca8c75690bf6ea8bc99
MonsterTeddy <48380512+MonsterTeddy@users.noreply.github.com>
2020-03-30 21:49:13 +0200

    maybe baby

--- server.js
+++ server.js
@@ -4,14 +4,8 @@
 const express = require('express');
 const app = express();
 
-// import lispum middleware
-const lorem = require('./api/lipsum.js');
-
 // set Port variable
 const PORT = process.env.PORT || 5000;
-
-// set api redirect to envoke lorem function
-app.get('/generator', (req, res) => lorem(req, res));
 
 // set redirect to public folder
 app.use(express.static('./public/'));
