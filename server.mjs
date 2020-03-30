b8e601ab8c36c260127b81021b390174bb6e7321
MonsterTeddy <48380512+MonsterTeddy@users.noreply.github.com>
2020-03-31 00:17:31 +0200

    updated to es6 imports

--- server.mjs
+++ server.mjs
@@ -1,14 +1,20 @@
 /** @format */
 
 // import & setup express
-const express = require('express');
+import express from 'express';
 const app = express();
+
+// import lorem
+import lorem from './api/index';
 
 // set Port variable
 const PORT = process.env.PORT || 5000;
 
 // set redirect to public folder
 app.use(express.static('./public/'));
+
+// invoke api on request
+app.get('/api', (req, res) => lorem(req, res));
 
 // start server
 app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
