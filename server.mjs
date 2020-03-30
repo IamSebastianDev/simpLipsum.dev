b8980a61d9dc5892dd5e36c5a27bf40a329265d9
MonsterTeddy <48380512+MonsterTeddy@users.noreply.github.com>
2020-03-30 21:22:56 +0200

    fix?

--- server.mjs
+++ server.mjs
@@ -5,7 +5,7 @@ import express from 'express';
 const app = express();
 
 // import lispum middleware
-import { lorem } from './api/lipsum.mjs';
+import { lorem } from './_api/lipsum.mjs';
 
 // set Port variable
 const PORT = process.env.PORT || 5000;
