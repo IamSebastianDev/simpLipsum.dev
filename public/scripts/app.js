cb5952bdbee1f86d8c827e4fc7345879e868edca
MonsterTeddy <48380512+MonsterTeddy@users.noreply.github.com>
2020-03-31 16:59:45 +0200

    finished up api docs and rest of design

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,16 +13,16 @@ let output = document.querySelector('.generator-Output');
 
 const generate = event => {
 	// check if the generate button was clicked
-	if (event.target.matches('.generator-GenerateButton')) {
+	if (event.target.closest('.generator-GenerateButton')) {
 		// get values to construct fetch data
-		let amout = document.querySelector(
+		let amount = document.querySelector(
 			".generator-Controls input[type='number']"
 		).value;
 
 		let type = document.querySelector('.generator-Controls select').value;
 
 		// fetch data and append to output
-		fetch(`https://simplipsum.now.sh/api?words=20`)
+		fetch(`https://simplipsum.now.sh/api?${type}=${amount}`)
 			.then(res => res.json())
 			.then(data => (output.innerHTML = data.text))
 			.catch(err => console.log(err));
@@ -31,3 +31,64 @@ const generate = event => {
 
 // add event listener
 window.addEventListener('click', generate);
+
+// copy function
+
+const copy = () => {
+	// check if copy button was clicked
+	if (event.target.closest('.generator-CopyButton')) {
+		let src = output.innerHTML;
+
+		console.log(src);
+
+		if (navigator.clipboard) {
+			let data = [
+				new ClipboardItem({
+					'text/plain': new Blob([src], {
+						type: 'text/plain'
+					})
+				})
+			];
+
+			navigator.clipboard.write(data);
+		} else {
+			// fallback for Safari mostly, which doesn't support the clipboard api.
+			// and obviouly IE, but who cares.
+
+			let data = document.createElement('textarea');
+
+			data.style =
+				'display: hidden; position: fixed: top: 0; left: 0; z-index: -1000; visibilty: none;';
+			document.querySelector('body').appendChild(data);
+			data.value = src;
+
+			data.focus();
+			data.select();
+			data.setSelectionRange(0, 99999); /*For mobile devices*/
+
+			/* Copy the text inside the text field */
+			document.execCommand('copy');
+
+			data.remove();
+		}
+	}
+};
+
+// add event listener
+window.addEventListener('click', copy);
+
+// menu
+
+let menu = document.querySelector('.nav-Bar ul');
+
+window.addEventListener('click', event => {
+	if (event.target.closest('.nav-MenuOpenBttn')) {
+		menu.style.transform = 'translateY(0px)';
+		document.querySelector('html').style.overflowY = 'hidden';
+	}
+
+	if (event.target.closest('.nav-MenuCloseBttn')) {
+		menu.style.transform = 'translateY(-100%)';
+		document.querySelector('html').style.overflowY = 'unset';
+	}
+});
